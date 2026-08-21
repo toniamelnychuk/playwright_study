d0405255d8415d7d410bd940117fd3f8d73c36fe
tonyamelnychuk <to.melnychuk@gmail.com>
2026-08-21 17:16:19 +0200

    init

---
+++ src/main/java/org/example/LoginPageTests.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginPageTests {
+
+    // Shared between all tests in this class.
+    static Playwright playwright;
+    static Browser browser;
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    void enterCreds(String login, String password) {
+        page.navigate("http://localhost:3000/src/main/resources/login.html");
+        page.locator("input[id=\"username\"]").fill(login);
+        page.locator("input[id=\"password\"]").fill(password);
+    }
+
+    @Test
+    void shouldEnterCorrectCredentials() {
+        enterCreds("to@mail.com", "toto");
+        page.locator("button").click();
+        assertEquals("Login successful", page.locator("div[id=\"message\"]").textContent());
+    }
+
+    @Test
+    void shouldEnterEmptyCredentials() {
+        enterCreds("", ""); // or skip this step
+        page.locator("button").click();
+        assertEquals("Username and password are required", page.locator("div[id=\"message\"]").textContent());
+    }
+
+    @Test
+    void shouldEnterIncorrectCredentials() {
+        enterCreds("incorrect@mail.com", "no"); // or skip this step
+        page.locator("button").click();
+        assertEquals("Invalid username or password", page.locator("div[id=\"message\"]").textContent());
+    }
+
+}
